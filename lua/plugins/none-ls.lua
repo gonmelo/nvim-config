e9278c9acb1146f158fecb52dd3699875fc3b416
Goncalo de Melo <gonvm1@gmail.com>
2024-03-30 02:41:50 +0000

    add linters and formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.isort, -- sort imports in python
+        null_ls.builtins.formatting.black, -- python formatter
+        null_ls.builtins.diagnostics.pylint, -- python linter
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
