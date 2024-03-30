b01700a3a89433999a2bbbdec249c250c1a90ba3
Goncalo de Melo <gonvm1@gmail.com>
2024-03-30 17:38:54 +0000

    add autocompletions and main menu

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,36 +1,48 @@
 return {
-  -- Mason
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        PATH = "prepend", -- "skip" seems to cause the spawning error
+	-- Mason
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				PATH = "prepend", -- "skip" seems to cause the spawning error
+			})
+		end,
+	},
+	-- Mason-lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "jsonls", "marksman", "pyright" },
+			})
+		end,
+	},
+	-- Neovim-lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities
       })
-    end,
-  },
-  -- Mason-lspconfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "jsonls", "marksman", "pyright" },
+			lspconfig.clangd.setup({
+        capabilities = capabilities
       })
-    end,
-  },
-  -- Neovim-lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.pyright.setup({})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+			lspconfig.marksman.setup({
+        capabilities = capabilities
+      })
+			lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
