ae556d27207ec3a54ec29cec6c1a063801da47a0
Goncalo de Melo <gonvm1@gmail.com>
2024-03-30 01:57:29 +0000

    add none-ls and fix identation

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,37 @@
 return {
-    -- Mason
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    -- Mason-lspconfig
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "jsonls", "marksman", "pyright" }
-            })
-        end
-    },
-    -- Neovim-lspconfig
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.jsonls.setup({})
-            lspconfig.marksman.setup({})
-            lspconfig.pyright.setup({})
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+  -- Mason
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        PATH = "prepend", -- "skip" seems to cause the spawning error
+      })
+    end
+  },
+  -- Mason-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd", "jsonls", "marksman", "pyright" }
+      })
+    end
+  },
+  -- Neovim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.marksman.setup({})
+      lspconfig.pyright.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
 
 }
